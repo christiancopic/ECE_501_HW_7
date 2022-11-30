8deb7e779794bd5f6209253b1f328ff45cd56748
christiancopic <silvercopic@gmail.com>
2022-11-30 16:52:30 -0700

    init commit

---
+++ prob6.py
@@ -0,0 +1,48 @@
+'''
+This code is super gross but it works. Sorry in advance.
+'''
+
+import numpy as np
+from matplotlib import pyplot as plt
+from numpy.linalg import eig
+
+def main():
+    #Define probabilities
+    a = 1/6
+    b = 19/120
+    c = 7/40
+    d = 1/120
+    #Let columns be starting square
+    #Let rows be ending square
+    M = np.array([[d, d, d, d, d, d, c, c, c, c, c, c], 
+                  [a, 0, 0, 0, 0, 0, 0, a, a, a, a, a], 
+                  [a, a, 0, 0, 0, 0, 0, 0, a, a, a, a], 
+                  [a, a, a, a, a, a, a, a, a, a, a, a], 
+                  [a, a, a, a, 0, 0, 0, 0, 0, 0, a, a],
+                  [b, b, b, b, b, 0, 0, 0, 0, 0, 0, b],
+                  [a, a, a, a, a, a, 0, 0, 0, 0, 0, 0],
+                  [0, a, a, a, a, a, a, 0, 0, 0, 0, 0],
+                  [0, 0, a, a, a, a, a, a, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, a, a, a, a, a, a, 0, 0],
+                  [0, 0, 0, 0, 0, b, b, b, b, b, b, 0]
+                  ])
+
+    eval, evec = eig(M)
+    max_mag = 0
+    index = -1
+    for i in range(len(eval)):
+        if abs(eval[i]) > max_mag:
+            max_mag = eval[i]
+            index = i
+    
+    mag_eig_sln = (abs(evec[:,index]))
+    norm_mag_eig_sln = mag_eig_sln / sum(mag_eig_sln)
+    print(mag_eig_sln)
+    for a, b in enumerate(norm_mag_eig_sln):
+        print('Space: ' + str(a + 1) + ': Probability: ' + str(b))
+
+
+
+if __name__ == '__main__':
+    main()
